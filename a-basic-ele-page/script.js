2119008857811d457dd2e5dcb168b0e93aff037e
J-Atulya <atulyajayakrishnan@gmail.com>
2023-07-21 18:52:52 +0530

    Added JS for input message

--- a-basic-ele-page/script.js
+++ a-basic-ele-page/script.js
@@ -1,3 +1,4 @@
+/* change bg colors */
 const colors = [ 'rgb(244,245,127', '#a5e004', '#e5a9db', '#756c9b', '#afd6ab']
 const button = document.querySelector('button')
 
@@ -5,3 +6,28 @@ button.addEventListener('click', function (){
     const index = parseInt(Math.random()*colors.length)
     document.querySelector('body').style.backgroundColor = colors[index]
 })
+
+/* message input box*/
+const form = document.querySelector("#msg-form")
+form.addEventListener('submit', submitted)
+
+function submitted(event){
+    event.preventDefault()
+
+    const msg = document.querySelector('#msg')
+    const error = document.querySelector('.error')
+    const msgCont = document.querySelector('.msg-cont')
+
+    if(msg.value === ''){
+        error.classList.add('show')
+        setTimeout(function (){
+            error.classList.remove('show')
+        }, 3000)
+        console.log("Type something ppl")
+    }
+    else{
+        msgCont.textContent = msg.value
+        msg.value = ''
+    }
+}
+
