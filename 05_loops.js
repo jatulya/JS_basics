fb4fcd08b252a2311274c1847b75c2e5b34ae1e4
J-Atulya <atulyajayakrishnan@gmail.com>
2023-09-09 23:34:40 +0530

    Loops

---
+++ 05_loops.js
@@ -0,0 +1,22 @@
+//for of loops
+for (let a of "Raniganj")
+  console.log(a)
+/*for in loops
+obj2 is defined in the file 02_dtype_obj.js
+for (let a in obj2)
+  console.log("Student: " + a + ` Their Marks: ${obj2[a]}`) */
+for (let a in "Ranigang")
+ console.log(a) 
+
+/*while 
+let i ,crct_num = 47 //i is undefined
+while(true){
+    i = prompt("Enter your number: ")
+    if (i == crct_num){
+        console.log("You have entered the correct number")
+        break
+    }
+    else
+        console.log("Try again...")
+} 
+this wont work on vscode  */
