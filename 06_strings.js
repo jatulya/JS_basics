56c3993166962abb6b7ff7c9667f077b238cfd78
J-Atulya <atulyajayakrishnan@gmail.com>
2023-09-09 23:59:49 +0530

    String declaration

---
+++ 06_strings.js
@@ -0,0 +1,8 @@
+let name1 = "Atulya"
+let name2 = 'Shenu'
+console.log(typeof(name1))
+console.log(name2.length)
+//template literals -> we can add "", '' and specify variable directly, without the using the + sign
+//${varname} -> this usage is called string interpalation
+let sentence = `Nikhil's Friend's name is "Pramod"` 
+console.log(sentence)
