b8c5ccf28ca2125eb4c4afa2f8fe804e7758661d
J-Atulya <atulyajayakrishnan@gmail.com>
2023-09-10 23:30:07 +0530

    String methods

--- 06_strings.js
+++ 06_strings.js
@@ -1,7 +1,5 @@
 let name1 = "Atulya"
-let name2 = 'Shenu'
 console.log(typeof(name1))
-console.log(name2.length)
 //template literals -> we can add "", '' and specify variable directly, without the using the + sign
 //${varname} -> this usage is called string interpalation
 let sentence = `Nikhil's Friend's name is "Pramod"` 
@@ -13,3 +11,20 @@ console.log(name1)
 console.log("Hello \n World\r") // new line character
 console.log("usage of escape char r -> let's see what \r happens")
 //brings the curson to the beginning of the line ->here it prints "happens escape char r -> let's see"
+
+//some string methods
+//for more methods, search mdn string methods
+let name2 = 'Shenu\'s'
+console.log(name2.length) //length of the string = 7 (\' is an escape seq->single char)
+console.log('hello'.toUpperCase()) //'HELLO'
+name2 = "Shrenu Shri"
+console.log(name2.slice(3,8)) //prints "enu S"
+console.log(name2.slice(4))// prints "nu Shri"
+console.log(name2.replace('Shri',"Goenka")) //prints Shrenu Goenka
+console.log(name2) //prints Shrenu Shri -> to make the changes permanent, assign the modified value
+name2 = "   Shrenu Pa riakh   "
+name2 = name2.trim()  //removes whitespaces from the end
+console.log(name2)
+
+//strings are immutable, eg -> we can't do name2[9] = 's' like that
+console.log("HaR\"".length)
