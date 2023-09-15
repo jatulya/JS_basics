3ff7dd0c2f504f175583a6a5254fb56bc9e1f1ed
J-Atulya <atulyajayakrishnan@gmail.com>
2023-09-15 20:30:35 +0530

    Array Methods

--- 07_arrays.js
+++ 07_arrays.js
@@ -1,4 +1,4 @@
-let ar = [1, 2, 3, null, "not present"]
+/*let ar = [1, 2, 3, null, "Zorca","not present"]
 console.log(ar) //prints [1,2,3,null,"not present"]
 let b = ar.toString()
 console.log(b) //prints 1,2,3,,not present
@@ -11,3 +11,13 @@ console.log(ar)
 console.log(ar.shift()) // removes first element and returns it, modifies the array
 delete ar[2] //just deletes the element at the index specified, but the length doesnt change
 console.log("after delete",ar) //after delete [ 2, 3, <1 empty item>, 'not present' ]
+console.log("after sorting: ", ar.sort()) //[ 2, 3, 'not present', <1 empty item> ]*/
+b = [1,2,36,78,9,8,4]
+b.sort() //alphabetical sorting
+console.log(b) // o/p -> [1,2,36,4,78,8,9]
+let tosort = (a,b) => a - b
+console.log(b.sort(tosort))
+b.splice(4, 3, "Hannah", null, 55, true)
+console.log(b) //[ 1, 2, 4, 8, 'Hannah', null, 55, true ]
+console.log(b.slice(3)) //[8, 'Hannah', null, 55, true]
+console.log(b.slice(3, 6)) // [8, 'Hannah', null]
