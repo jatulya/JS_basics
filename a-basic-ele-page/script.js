05217d46c694f925fa3e1cf0c2b130444b8b1bf9
J-Atulya <atulyajayakrishnan@gmail.com>
2023-11-20 23:59:06 +0530

    Added supabase

--- a-basic-ele-page/script.js
+++ a-basic-ele-page/script.js
@@ -12,7 +12,7 @@ const form = document.querySelector("#msg-form")
 form.addEventListener('submit', submitted)
 
 function submitted(event){
-    event.preventDefault()
+    event.preventDefault() //prevents the automatic refreshing of the page and performs the required actions
 
     const msg = document.querySelector('#msg')
     const error = document.querySelector('.error')
@@ -23,11 +23,30 @@ function submitted(event){
         setTimeout(function (){
             error.classList.remove('show')
         }, 3000)
-        console.log("Type something ppl")
     }
     else{
         msgCont.textContent = msg.value
         msg.value = ''
     }
+    const { createClient } = supabase
+supabase = createClient('https://taztytueelhdpsfjvscg.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhenR5dHVlZWxoZHBzZmp2c2NnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1MDM1MjMsImV4cCI6MjAxNjA3OTUyM30.H7BNKOkvn8e_9CmNKxI6gsdnWm_Zo218bSlS_xzNiU0')
+
+    const form = document.querySelector('#msg-form') //selected the form
+    form.addEventListener('submit', async(event) => { 
+        event.preventDefault() 
+        
+        const formips = form.querySelectorAll('input, textarea, select')
+        let datas = {}
+        formips.forEach(element => {
+            const {value, name} = element //selected object has properties value and name
+            if (value) {
+                datas[name] = value
+            }
+        })
+
+        const { error, data}=await supabase.from('msgcontent').insert([datas])
+        console.log(error, data)
+
+    })
 }
 
